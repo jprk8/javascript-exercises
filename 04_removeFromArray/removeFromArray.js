b0e15de2cb32b7be0b647e56e225f2389a25f9a3
John Park <176120986+jprk8@users.noreply.github.com>
2024-07-26 16:02:11 -0700

    Finish exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray, ...args) {
+    const newArray = [];
+    const items = [...args];
+    outer: for (let i = 0; i < myArray.length; i++) {
+        for (let j = 0; j < items.length; j++) {
+            if (myArray[i] === items[j]) {
+                continue outer;
+            }
+        }
+        newArray.push(myArray[i]);
+    }
+    return newArray;
 };
 
 // Do not edit below this line
