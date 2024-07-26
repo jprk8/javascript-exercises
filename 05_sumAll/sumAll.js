c58b08aacee0d23372b1dae45be423331898f817
John Park <176120986+jprk8@users.noreply.github.com>
2024-07-26 16:19:51 -0700

    Finish exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(first, last) {
+    let sum = 0;
+    if (first < 0 || last < 0 || !(Number.isInteger(first)) || !(Number.isInteger(last))) {
+        return 'ERROR';
+    }
+    if (first > last) {
+        let temp = first;
+        first = last;
+        last = temp;
+    }
+    for (let i = first; i <= last; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
