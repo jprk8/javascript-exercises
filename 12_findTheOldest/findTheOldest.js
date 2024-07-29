3b23e7462e2ea4153820f932a4e81c457f7261de
John Park <176120986+jprk8@users.noreply.github.com>
2024-07-29 16:18:42 -0700

    Finish exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let newArr = people.sort((a, b) => {
+        const currentYear = new Date().getFullYear();
+        if (!a.yearOfDeath) aDeath = currentYear;
+        else aDeath = a.yearOfDeath;
+        if (!b.yearOfDeath) bDeath = currentYear;
+        else bDeath = b.yearOfDeath;
+
+        const ageA = aDeath - a.yearOfBirth;
+        const ageB = bDeath - b.yearOfBirth;
+        return ageA > ageB ? -1 : 1;
+    });
+    return newArr[0];
 
 };
 
