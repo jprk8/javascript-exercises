7d0f6e851cfd1aac2227829ccec2ddfc84c24fbf
John Park <176120986+jprk8@users.noreply.github.com>
2024-07-29 15:16:55 -0700

    Finish exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    const fib = [0, 1];
+    if (index < 0) {
+        return 'OOPS';
+    } else {
+        for (let i = 1; i <= index; i++) {
+            fib.push(fib[i]+fib[i-1]);
+        }
+        return fib[index];
+    }
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
