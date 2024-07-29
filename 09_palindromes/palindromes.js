2ec2e537e7cb4af1e35cbb7a67ec306a399b4ede
John Park <176120986+jprk8@users.noreply.github.com>
2024-07-29 14:57:14 -0700

    Finish exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,28 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    //checking for palindrome...with extra steps...
+    //remember reverse() exists
+    const punctuation = `.,?!'":; `;
+    let arr = word.split("");
+    let filtered = arr
+        .filter((char) => {
+            if (!punctuation.includes(char)) {
+                return char;
+        }})
+        .map((item) => {
+            return item.toLowerCase();
+        });
+    const middle = Math.floor(filtered.length / 2);
+    let last = filtered.length - 1;
+    let palindrome = true;
+    for (let i = 0; i <= middle; i++) {
+        if (filtered[i] != filtered[last]) {
+            palindrome = false;
+        }
+        last--;
+    }
+    return palindrome;
+}
 
-};
 
 // Do not edit below this line
 module.exports = palindromes;
